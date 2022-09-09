b574ffff4c478bff31a3980ef9ca775362cdcd44
XRow08 <murilloaugustoe081@gmail.com>
2022-09-08 22:37:45 -0300

    1commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+})
